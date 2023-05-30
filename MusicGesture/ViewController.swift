07676834a4684c6ff2d2f441b2a10780ade6f04b
Mert Gürcan <mertgurcan@alumni.bilkent.edu.tr>
2023-05-30 22:28:27 +0300

    Initial Commit

---
+++ MusicGesture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicGesture
+//
+//  Created by Mert Gürcan on 30.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
